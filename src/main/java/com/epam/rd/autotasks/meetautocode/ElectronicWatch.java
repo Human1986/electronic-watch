a7ff10fb5c95fca76abee6ea6632c838c2d2179d
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-05-13 19:08:00 +0000

    1

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -7,6 +7,15 @@ public class ElectronicWatch {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
 
+        int H = seconds / 60;
+        int minute = H % 60;
+        int sec = seconds % 60;
+        H = H / 60;
+
+        H = H == 24 ? 0 : H;
+
+        System.out.printf("%d:%02d:%02d", H, minute, sec);
+
 
     }
 }
